66b4768edcfaae42a105bc4563cba0e6e2236c62
Dominic Aglialoro <domiaglialoro@gmail.com>
2023-03-03 22:32:52 -0800

    Add scripts

---
+++ Scripts/USA_Scene.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using SRXDCustomVisuals.Core;
+using UnityEngine;
+
+namespace SRXDBackgrounds.USA {
+    public class USA_Scene : MonoBehaviour {
+        [SerializeField] private USA_WaveLines waveLines;
+        [SerializeField] private USA_PlanetRings planetRings;
+        [SerializeField] private USA_Book book;
+        [SerializeField] private USA_LaserRing laserRing;
+        [SerializeField] private USA_Particles particles;
+        [SerializeField] private USA_Earth earth;
+            
+        private VisualsEventReceiver eventReceiver;
+        
+        private void Awake() {
+            eventReceiver = GetComponent<VisualsEventReceiver>();
+            eventReceiver.On += EventOn;
+            eventReceiver.Off += EventOff;
+            eventReceiver.ControlChange += EventControlChange;
+            eventReceiver.Reset += EventReset;
+        }
+
+        private void EventOn(VisualsEvent obj) {
+            
+        }
+
+        private void EventOff(VisualsEvent obj) {
+            
+        }
+
+        private void EventControlChange(VisualsEvent obj) {
+            
+        }
+
+        private void EventReset() {
+            
+        }
+    }
+}
