653a0f47d5e737bcc9532eafffbd8436daf53409
Dominic Aglialoro <domiaglialoro@gmail.com>
2023-03-24 22:45:56 -0700

    Update Plugin

--- Scripts/Plugin.cs
+++ Scripts/Plugin.cs
@@ -5,7 +5,7 @@ namespace SRXDBackgrounds.USA {
     [BepInDependency("srxd.customvisuals", "1.2.5")]
     [BepInDependency("srxd.backgrounds.common","1.0.2")]
     [BepInPlugin("srxd.backgrounds.usa", "srxdbackgrounds.usa", "1.0.1")]
-    public class Plugin : MonoBehaviour {
+    public class Plugin : BaseUnityPlugin {
         private void Awake() => Destroy(gameObject);
     }
 }
